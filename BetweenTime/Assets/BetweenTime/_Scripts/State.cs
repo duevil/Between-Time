d162aacc3153a8816cab0689e1399110d179f058
u38524 <u38524@hs-harz.de>
2024-11-06 16:54:06 +0100

    game controller states first approach

---
+++ BetweenTime/Assets/BetweenTime/_Scripts/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BetweenTime._Scripts
+{
+    public interface IState{}
+    
+    [Serializable]
+    public class State<T> : IState
+    {
+        private T _value;
+        public UnityEvent<T> onChanged = new();
+
+        public T value
+        {
+            get => _value;
+            set
+            {
+                if (!Equals(value, _value)) onChanged?.Invoke(_value);
+                _value = value;
+            }
+        }
+    }
+}
