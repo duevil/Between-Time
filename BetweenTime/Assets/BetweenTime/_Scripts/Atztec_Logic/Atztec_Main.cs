4aa1258e746f990bbb601286b777ed8b605cd9c3
u38524 <u38524@hs-harz.de>
2024-11-20 13:19:21 +0100

    highlighting and buttons

---
+++ BetweenTime/Assets/BetweenTime/_Scripts/Atztec_Logic/Atztec_Main.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Atztec_Main : MonoBehaviour
+{
+    bool sync;
+    bool numbersSolved;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enableNumPad();
+    }
+
+    void enableNumPad()
+    {
+
+    }
+}
---
+++ BetweenTime/Assets/BetweenTime/_Scripts/Atztec_Logic/Atztec_Numbers.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Atztec_Numbers : MonoBehaviour
+{
+    int count;
+    string solution;
+    string input;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // grabable machen
+        // erscheinen lassen
+        print("Button pressed");
+        input = "";
+    }
+
+    void buttonPressed(string number)
+    {
+        input += number;
+
+        // if Rätsel gelöst -> weiter
+    }
+}
