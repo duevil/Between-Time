a7d0f39d34169712d4003f9017656db68381da1b
Malte <u37520@hs-harz.de>
2024-12-12 11:17:55 +0100

    Modified models and lighting

--- BetweenTime/Assets/BetweenTime/_Scripts/SkyboxRotation.cs
+++ BetweenTime/Assets/BetweenTime/_Scripts/SkyboxRotation.cs
@@ -16,13 +16,27 @@ namespace BetweenTime._Scripts
         [Tooltip("The speed at which the skybox rotates")] [SerializeField] [ReadOnly]
         private float speed;
 
+        // Copied instance of the RenderSettings' skybox to apply the rotation override to
+        private Material _skybox;
+
+        /// <summary>
+        ///     Copies the current skybox from the RenderSettings,
+        ///     stores the copy and sets it as the new RenderSettings' skybox
+        /// </summary>
+        private void Awake()
+        {
+            var skybox = RenderSettings.skybox;
+            _skybox = new Material(skybox);
+            RenderSettings.skybox = _skybox;
+        }
+
         /// <summary>
         ///     Updates the skybox rotation based on the current time
         /// </summary>
         private void Update()
         {
             speed = speedFactor == 0 ? 0f : 0.01f * Mathf.Pow(10f, speedFactor - 1);
-            RenderSettings.skybox.SetFloat(Rotation, Time.time * speed);
+            _skybox.SetFloat(Rotation, Time.time * speed);
         }
     }
 }
