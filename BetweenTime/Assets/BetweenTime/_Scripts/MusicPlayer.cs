f685e1ce90fe0e905f8f84cc3c53a80e8e8259ce
Malte <maltekasolowsky@gmail.com>
2024-12-15 23:15:55 +0100

    Victory sound, sockets, bevels, cleanup - Added victory sound - Implemented usage of different socket models - Added bevels to sockets and steampunk machine - Deleted .blend1-files

--- BetweenTime/Assets/BetweenTime/_Scripts/MusicPlayer.cs
+++ BetweenTime/Assets/BetweenTime/_Scripts/MusicPlayer.cs
@@ -3,25 +3,45 @@ using UnityEngine;
 
 namespace BetweenTime._Scripts
 {
+    /// <summary>
+    ///     Controls the music player in the game
+    /// </summary>
     public class MusicPlayer : MonoBehaviour
     {
-        private AudioSource _audioSource;
-
+        [Tooltip("The music to play when the game is won")] [SerializeField]
+        private AudioClip victoryMusic; 
+        
+        private AudioSource _audioSource; // The audio source component of the music player
+        
+        /// <summary>
+        ///     Initializes the audio source component on enable
+        /// </summary>
         private void Start()
         {
             _audioSource = GetComponent<AudioSource>();
             _audioSource.clip.LoadAudioData();
         }
 
+        /// <summary>
+        ///     Adds a listener to the main state change event;
+        ///     plays the normal music when the game enters any running state,
+        ///     stops the music when the game is idle
+        ///     and plays the victory music when the game is won
+        /// </summary>
+        /// <param name="value"></param>
         public void MainStateLister(MainState value)
         {
             switch (value)
             {
-                case MainState.Idle or MainState.GameWon:
+                case MainState.Idle:
                     _audioSource.Stop();
                     break;
                 case MainState.GameLost:
                     // do nothing
+                    break;
+                case MainState.GameWon:
+                    _audioSource.clip = victoryMusic;
+                    _audioSource.Play();
                     break;
                 default:
                     if (!_audioSource.isPlaying) _audioSource.Play();
