23199cff05973bf0edb27fd3d1df634a59f0bf22
Malte <maltekasolowsky@gmail.com>
2024-12-19 01:07:04 +0100

    Fixed pipe collider, updated second book page

--- BetweenTime/Assets/BetweenTime/_Scripts/steampunk/MazeBuilder.cs
+++ BetweenTime/Assets/BetweenTime/_Scripts/steampunk/MazeBuilder.cs
@@ -5,32 +5,28 @@ namespace BetweenTime._Scripts.steampunk
 {
     public class MazeBuilder : MonoBehaviour
     {
+        [SerializeField] private GameObject pipeCurved;
 
-        [SerializeField]
-        private GameObject pipeCurved;
+        [SerializeField] private GameObject pipeStraight;
 
-        [SerializeField]
-        private GameObject pipeStraight;
+        [SerializeField] private GameObject pipeStraightEnd;
 
-        [SerializeField]
-        private GameObject pipeStraightEnd;
+        [SerializeField] private GameObject pipeXJunction;
 
-        [SerializeField]
-        private GameObject pipeXJunction;
+        [SerializeField] private GameObject pipeTJunction;
 
-        [SerializeField]
-        private GameObject pipeTJunction;
-
-        private readonly char[,] _labyrinth = {
-            {'3', 'b', '9', '1', '5', '5', '5', '3'},
-            {'a', 'c', '2', 'c', '5', '3', '9', '6'},
-            {'c', '3', 'c', '3', '9', '6', 'a', 'b'},
-            {'b', 'a', 'd', '6', 'c', '3', 'a', 'a'},
-            {'a', 'c', '3', '9', '3', 'a', 'c', '2'},
-            {'8', '5', '6', 'a', 'c', '6', 'd', '2'},
-            {'c', '3', 'd', '4', '5', '5', '3', 'a'},
-            {'d', '4', '5', '5', '5', '5', '6', 'c'}
+        private readonly char[,] _labyrinth =
+        {
+            { '3', 'b', '9', '1', '5', '5', '5', '3' },
+            { 'a', 'c', '2', 'c', '5', '3', '9', '6' },
+            { 'c', '3', 'c', '3', '9', '6', 'a', 'b' },
+            { 'b', 'a', 'd', '6', 'c', '3', 'a', 'a' },
+            { 'a', 'c', '3', '9', '3', 'a', 'c', '2' },
+            { '8', '5', '6', 'a', 'c', '6', 'd', '2' },
+            { 'c', '3', 'd', '4', '5', '5', '3', 'a' },
+            { 'd', '4', '5', '5', '5', '5', '6', 'c' }
         };
+
         private const int LabySize = 8;
 
         private const float CellToCelldistance = 0.3738f;
@@ -46,38 +42,44 @@ namespace BetweenTime._Scripts.steampunk
             var pos = gameObject.transform.position;
             var startZ = pos.z;
 
-            for(var i = 0; i<LabySize; i++)
+            for (var i = 0; i < LabySize; i++)
             {
-                for (var j = 0; j<LabySize; j++)
+                for (var j = 0; j < LabySize; j++)
                 {
-                    var pipeType = _labyrinth[i,j];
+                    var pipeType = _labyrinth[i, j];
                     GameObject pipe = null;
 
                     switch (pipeType)
                     {
                         case '1':
                             pipe = PipeType1();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case '2':
                             pipe = PipeType2();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case '3':
                             pipe = PipeType3();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case '4':
                             pipe = PipeType4();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case '5':
                             pipe = PipeType5();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case '6':
                             pipe = PipeType6();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         /*
                     case '7':
@@ -87,33 +89,40 @@ namespace BetweenTime._Scripts.steampunk
                     */
                         case '8':
                             pipe = PipeType8();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case '9':
                             pipe = PipeType9();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case 'a':
                             pipe = PipeTypeA();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case 'b':
                             pipe = PipeTypeB();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case 'c':
                             pipe = PipeTypeC();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                         case 'd':
                             pipe = PipeTypeD();
-                            Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation, gameObject.transform);
+                            pipe = Instantiate(pipe, new Vector3(pos.x, pos.y, pos.z), pipe.transform.rotation,
+                                gameObject.transform);
                             break;
                     }
 
                     pipe?.AddComponent<BoxCollider>();
                     pos = new Vector3(pos.x, pos.y, pos.z + CellToCelldistance);
                 }
+
                 pos = new Vector3(pos.x, pos.y - CellToCelldistance, startZ);
             }
         }
@@ -159,13 +168,13 @@ namespace BetweenTime._Scripts.steampunk
             pipe.transform.rotation = Quaternion.Euler(0, -90, -90);
             return pipe;
         }
-        
-    /*
-        private GameObject PipeType7()
-        {
 
-        }
-    */
+        /*
+            private GameObject PipeType7()
+            {
+
+            }
+        */
 
         private GameObject PipeType8()
         {
